a41a1a93548373221964195cca26977d84559aca
kwin0825 <62418972+chris0825@users.noreply.github.com>
2022-01-16 17:15:49 +0900

    Update 1003.java

--- Dynamic Programming/1003.java
+++ Dynamic Programming/1003.java
@@ -8,31 +8,24 @@
 import java.io.*;
 
 public class Main {
-    static int cnt0, cnt1;
-    static int fibo[][] = new int[41][2];
+    static int dp[][] = new int[41][2];
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int tC = Integer.parseInt(br.readLine());
-        int n;
-        for(int i=0; i<tC; i++){
-            cnt0 = 0;
-            cnt1 = 0;
-            n = Integer.parseInt(br.readLine());
-            fibonacci(n);
-            bw.write(fibo[n][0]+" "+fibo[n][1]+"\n");
+        while(tC-- > 0){
+            int fibo[] = fibonacci(Integer.parseInt(br.readLine()));
+            bw.write(fibo[0]+" "+fibo[1]+"\n");
         }
         bw.close();
     }
     static int[] fibonacci(int n) {
-        if(n == 0)
-            fibo[0][0] = 1;
-        else if(n == 1)
-            fibo[1][1] = 1;
-        else if(fibo[n][0] == 0 && fibo[n][1] == 0) {
-            fibo[n][0] = fibonacci(n-1)[0] + fibonacci(n-2)[0];
-            fibo[n][1] = fibonacci(n-1)[1] + fibonacci(n-2)[1];
+        if(n <= 1)
+            dp[n][n] = 1;
+        else if(dp[n][0] == 0 && dp[n][1] == 0) {
+            dp[n][0] = fibonacci(n-1)[0] + fibonacci(n-2)[0];
+            dp[n][1] = fibonacci(n-1)[1] + fibonacci(n-2)[1];
         }
-        return fibo[n];
+        return dp[n];
     }
 }
