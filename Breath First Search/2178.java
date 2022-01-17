53a8d3ab4b6f2b735fe5923a1ffb377473c20fc2
kwin0825 <62418972+chris0825@users.noreply.github.com>
2022-01-17 22:59:44 +0900

    Update 2178.java

--- Breath First Search/2178.java
+++ Breath First Search/2178.java
@@ -11,7 +11,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int cSize, rSize, visited[][];
+    static int cSize, rSize;
     static String maze[][];
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,44 +20,37 @@ public class Main {
         cSize = Integer.parseInt(size.nextToken());
         rSize = Integer.parseInt(size.nextToken());
         maze = new String[cSize][rSize];
-        visited = new int[cSize][rSize];
         for(int i=0; i<cSize; i++)
             maze[i] = br.readLine().split("");
-        bfs(0, 0);
-        bw.write(String.valueOf(visited[cSize-1][rSize-1]));
+        bw.write(bfs(0, 0)+"\n");
         bw.close();
     }
     static class Spot{
-        int x;
-        int y;
-        
-        Spot(int x, int y) {
-            this.x = x;
-            this.y = y;
+        int r;
+        int c;
+        Spot(int r, int c) {
+            this.r = r;
+            this.c = c;
         }
     }
-    static void bfs(int col, int row) {
+    static int bfs(int col, int row) {
         Queue<Spot> queue = new LinkedList<>();
+        int visited[][] = new int[cSize][rSize];
         queue.add(new Spot(col, row));
         visited[col][row] = 1;
+        int mR[] = {1, 0, -1, 0};
+        int mC[] = {0, 1, 0, -1};
         while(!queue.isEmpty()) {
             Spot s = queue.remove();
-            if(s.x+1 < cSize && visited[s.x+1][s.y]==0 && maze[s.x+1][s.y].equals("1")) {
-                queue.add(new Spot(s.x+1, s.y));
-                visited[s.x+1][s.y] = visited[s.x][s.y] + 1;
-            }
-            if(s.y+1 < rSize && visited[s.x][s.y+1]==0 && maze[s.x][s.y+1].equals("1")) {
-                queue.add(new Spot(s.x, s.y+1));
-                visited[s.x][s.y+1] = visited[s.x][s.y] + 1;
-            }
-            if(s.x-1 >= 0 && visited[s.x-1][s.y]==0 && maze[s.x-1][s.y].equals("1")) {
-                queue.add(new Spot(s.x-1, s.y));;
-                visited[s.x-1][s.y] = visited[s.x][s.y] + 1;
-            }
-            if(s.y-1 >= 0 && visited[s.x][s.y-1]==0 && maze[s.x][s.y-1].equals("1")) {
-                queue.add(new Spot(s.x, s.y-1));
-                visited[s.x][s.y-1] = visited[s.x][s.y] + 1;
-            }
+            for(int i=0; i<4; i++)
+                if(isArea(s.r+mR[i], s.c+mC[i]) && visited[s.r+mR[i]][s.c+mC[i]]==0 && maze[s.r+mR[i]][s.c+mC[i]].equals("1")) {
+                    queue.add(new Spot(s.r+mR[i], s.c+mC[i]));
+                    visited[s.r+mR[i]][s.c+mC[i]] = visited[s.r][s.c] + 1;
+                }
         }
+        return visited[cSize-1][rSize-1];
+    }
+    static boolean isArea(int col, int row) {
+        return col >= 0 && col < cSize && row >= 0 && row < rSize;
     }
 }
