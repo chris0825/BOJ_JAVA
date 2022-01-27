97eed36f4a8d01b60ec7f3273da7176355259213
Seunghyun._.KO <62418972+chris0825@users.noreply.github.com>
2022-01-27 23:22:11 +0900

    Update 1931.java

--- Sort/1931.java
+++ Sort/1931.java
@@ -6,6 +6,7 @@
     1. 그리디 알고리즘
     2. 정렬
 */
+// 1. 2차원 배열 사용
 import java.util.*;
 import java.io.*;
 
@@ -35,5 +36,43 @@ public class Main {
                 cnt++;
             }
         System.out.println(cnt);
+    }
+}
+
+// 2. 클래스 선언해서 사용
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Meeting mt[] = new Meeting[n];
+        for(int i=0; i<n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            mt[i] = new Meeting(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        }
+        Arrays.sort(mt);
+        int cnt = 0, eT = 0;
+        for(int i=0; i<n; i++)
+            if(eT <= mt[i].sT) {
+                eT = mt[i].eT;
+                cnt++;
+            }
+        System.out.println(cnt);
+    }
+    static class Meeting implements Comparable<Meeting>{
+        int sT;
+        int eT;
+        Meeting (int s, int e) {
+            this.sT = s;
+            this.eT = e;
+        }
+        @Override
+        public int compareTo(Meeting m) {
+            if(this.eT == m.eT)
+                return this.sT - m.sT;
+            return this.eT - m.eT;
+        }
     }
 }
