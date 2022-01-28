ca9a5f04e6da224fd21ece0e39f9f01d6f01917e
Seunghyun._.KO <62418972+chris0825@users.noreply.github.com>
2022-01-28 22:37:26 +0900

    Update 1931.java

--- Sort/1931.java
+++ Sort/1931.java
@@ -11,11 +11,10 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int mt[][];
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        mt = new int[n][2];
+        int mt[][] = new int[n][2];
         for(int i=0; i<n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             mt[i][0] = Integer.parseInt(st.nextToken());
