c5bbf62419b6e9ee10b6c751b7a6708da3af6b50
Seunghyun._.KO <62418972+chris0825@users.noreply.github.com>
2022-01-28 22:27:55 +0900

    Update 1697.java

--- Breath First Search/1697.java
+++ Breath First Search/1697.java
@@ -19,28 +19,29 @@ public class Main {
         System.out.println(hideAseek(subin, sister));
     }
     static int hideAseek(int start, int end) {
-        Queue<Integer> queue = new LinkedList<>();
+        Queue<Integer> queue = new LinkedList<>(); // 다음 경로를 저장할 큐
         queue.add(start);
-        int second[] = new int[100001];
+        int second[] = new int[100001]; // 경로 탐색에 걸린 시간 저장할 배열
         second[start] = 1;
+        // 시작 위치는 시간이 0이어야 맞지만 1로 저장해두는 이유는 0이면 방문 유무를 체크 할 때 방문하지 않았다고 판단되기 때문에 1로 시작한다.
         int temp;
         while(!queue.isEmpty()) {
             temp = queue.remove();
-            if(temp-1 >= 0 && second[temp-1] == 0) {
+            if(temp-1 >= 0 && second[temp-1] == 0) { // -1칸 이동
                 queue.add(temp-1);
                 second[temp-1] = second[temp] + 1;
             }
-            if(temp*2 <= 100000 && second[temp*2] == 0) {
+            if(temp*2 <= 100000 && second[temp*2] == 0) { // X2칸 이동
                 queue.add(temp*2);
                 second[temp*2] = second[temp] + 1;
             }
-            if(temp+1 <= 100000 && second[temp+1] == 0) {
+            if(temp+1 <= 100000 && second[temp+1] == 0) { // +1칸 이동
                 queue.add(temp+1);
                 second[temp+1] = second[temp] + 1;
             }
-            if(second[end] != 0)
+            if(second[end] != 0) // 동생의 위치를 찾았다면 경로 탐색 종료
                 break;
         }
-        return second[end]-1;
+        return second[end]-1; // 시작 위치의 시간을 1로 시작했기 때문에 해당 시간만큼 결과값에서 빼줘야 맞는 시간이 된다.
     }
 }
