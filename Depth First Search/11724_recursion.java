87904133fde6b1853f0869474320adff7759d977
Seunghyun._.KO <62418972+chris0825@users.noreply.github.com>
2022-01-31 14:22:01 +0900

    Update 11724_recursion.java

--- Depth First Search/11724_recursion.java
+++ Depth First Search/11724_recursion.java
@@ -12,35 +12,37 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static int n;
+    static boolean node[][], visited[];
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer init = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(init.nextToken());
+        n = Integer.parseInt(init.nextToken());
         int m = Integer.parseInt(init.nextToken());
-        boolean node[][] = new boolean[n+1][n+1];
-        int u, v;
+        node = new boolean[n+1][n+1];
+        int v1, v2;
         while(m-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            u = Integer.parseInt(st.nextToken());
-            v = Integer.parseInt(st.nextToken());
-            node[u][v] = true;
-            node[v][u] = true;
+            v1 = Integer.parseInt(st.nextToken());
+            v2 = Integer.parseInt(st.nextToken());
+            node[v1][v2] = true;
+            node[v2][v1] = true;
         }
-        boolean visit[] = new boolean[n+1];
+        visited = new boolean[n+1];
         int cnt = 0;
         for(int i=1; i<=n; i++)
-            if(!visit[i]) {
+            if(!visited[i]) {
                 cnt++;
-                connected(node, visit, i, n);
+                visited[i] = true;
+                connected(i);
             }
         System.out.println(cnt);
     }
-    static void connected(boolean node[][], boolean visit[], int idx, int num) {
-        for(int i=1; i<=num; i++){
-            if(node[idx][i] && !visit[i]) {
-                visit[i] = true;
-                connected(node, visit, i, num);
+    static void connected (int vertex) {
+        for(int i=1; i<=n; i++)
+            if(node[vertex][i] && !visited[i]) {
+                visited[i] = true;
+                connected(i);
             }
-        }
     }
 }
