625d4437f8ec3a72afa9be95bd6e2d6f10a68aa2
Seunghyun._.KO <62418972+chris0825@users.noreply.github.com>
2022-02-01 20:25:50 +0900

    Update 1541.java

--- Greedy/1541.java
+++ Greedy/1541.java
@@ -14,37 +14,31 @@ import java.io.*;
 public class Main {
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
+        String str = br.readLine();
         ArrayList<Integer> numL = new ArrayList<>();
         ArrayList<Character> operate = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
-        for(int i=0; i<input.length(); i++)
-            if(input.charAt(i)-'0' >= 0)
-                sb.append(input.charAt(i));
+        for(int i=0; i<str.length(); i++)
+            if(str.charAt(i)-'0' >= 0)
+                sb.append(str.charAt(i));
             else {
                 if(sb.length() != 0)
                     numL.add(Integer.parseInt(sb.toString()));
                 sb.delete(0, sb.length());
-                operate.add(input.charAt(i));
+                operate.add(str.charAt(i));
             }
         numL.add(Integer.parseInt(sb.toString()));
         int result = numL.get(0);
-        int temp;
-        for(int i=1; i<numL.size(); i++){
+        for(int i=1; i<numL.size(); i++)
             switch(operate.get(i-1)) {
                 case '+':
                     result += numL.get(i);
                     break;
                 case '-':
-                    temp = numL.get(i);
-                    i++;
-                    for(; i<numL.size(); i++) {
-                        temp += numL.get(i);
-                    }
-                    result -= temp;
+                    for(; i<numL.size(); i++)
+                        result -= numL.get(i);
                     break;
             }
-        }
         System.out.println(result);
     }
 }
