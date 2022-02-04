45804a107c8b45296335a1c1ddf623cc407ca734
Seunghyun._.KO <62418972+chris0825@users.noreply.github.com>
2022-02-04 23:22:36 +0900

    Create 11279_PriorityQueue_Reverse.java

---
+++ Data Structure/11279_PriorityQueue_Reverse.java
@@ -0,0 +1,28 @@
+/**
+  백준 11279번 - 최대 힙
+  https://www.acmicpc.net/problem/11279
+  
+  알고리즘 분류
+    1. 자료 구조
+    2. 우선순위 큐
+*/
+import java.util.*;  
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException{  
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        String command;
+        while(n-- > 0) {
+            command = br.readLine();
+            if(command.equals("0"))
+                bw.write(maxHeap.isEmpty() ? "0\n" : maxHeap.remove()+"\n");
+            else
+                maxHeap.add(Integer.parseInt(command));
+        }
+        bw.close();
+    }
+}
