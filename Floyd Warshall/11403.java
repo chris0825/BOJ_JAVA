ac18be0b9af2476b79bab37eb4bc008d7882d07e
Seunghyun._.KO <62418972+chris0825@users.noreply.github.com>
2022-02-05 20:39:05 +0900

    Update 11403.java

--- Floyd Warshall/11403.java
+++ Floyd Warshall/11403.java
@@ -11,28 +11,30 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int graph[][];
+    static int graph[][], num;
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int num = Integer.parseInt(br.readLine());
+        num = Integer.parseInt(br.readLine());
         graph = new int[num][num];
-        int temp;
         for(int i=0; i<num; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j=0; j<num; j++)
                 graph[i][j] = Integer.parseInt(st.nextToken());
         }
-        for(int i=0; i<num; i++)
-            for(int j=0; j<num; j++)
-                for(int k=0; k<num; k++)
-                    if(graph[j][k]==1 || (graph[j][i]==1  && graph[i][k]==1))
-			graph[j][k] = 1;
+        floyd_warshall();
 		for(int i=0; i<num; i++) {
 		    for(int j=0; j<num; j++)
 		        bw.write(graph[i][j]+" ");
 		    bw.newLine();
 		}
 		bw.close();
+    }
+    static void floyd_warshall() {
+        for(int i=0; i<num; i++)
+            for(int j=0; j<num; j++)
+                for(int k=0; k<num; k++)
+                    if(graph[j][k]==1 || (graph[j][i]==1  && graph[i][k]==1))
+		                graph[j][k] = 1;
     }
 }
