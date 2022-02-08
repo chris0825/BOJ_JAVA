5fe670324e5825c967d1d1e8b8ae6c375572ee21
Seunghyun._.KO <62418972+chris0825@users.noreply.github.com>
2022-02-08 20:14:59 +0900

    Create 1001.java

---
+++ Math/1001.java
@@ -0,0 +1,19 @@
+/**
+  백준 1001번 - A-B
+  https://www.acmicpc.net/problem/1001
+  
+  알고리즘 분류
+    1. 수학
+    2. 구현
+    3. 사칙연산
+*/
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        System.out.println(a-b);
+    }
+}
