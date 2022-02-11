065e4707beb8eca6bd31163c78ee86b1643dd001
Seunghyun._.KO <62418972+chris0825@users.noreply.github.com>
2022-02-11 23:41:25 +0900

    Create 1008.java

---
+++ Math/1008.java
@@ -0,0 +1,19 @@
+/**
+  백준 1008번 - A/B
+  https://www.acmicpc.net/problem/1008
+  
+  알고리즘 분류
+    1. 수학
+    2. 구현
+    3. 사칙연산
+*/
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        double a = input.nextInt();
+        double b = input.nextInt();
+        System.out.println(a/b);
+    }
+}
