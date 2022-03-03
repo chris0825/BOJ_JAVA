6fb6e8bad46a9cb6839d73fd7ee2186741d48319
Seunghyun._.KO <62418972+chris0825@users.noreply.github.com>
2022-03-03 22:35:51 +0900

    Update 9095.java

--- Dynamic Programming/9095.java
+++ Dynamic Programming/9095.java
@@ -11,24 +11,19 @@ public class Main {
     static int dp[] = new int[11];
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int tC = Integer.parseInt(br.readLine());
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 4;
-        int n;
-        while(tC-- > 0) {
-            n = Integer.parseInt(br.readLine());
-            bw.write(plus123(n)+"\n");
-        }
-        bw.close();
+        StringBuilder sb = new StringBuilder();
+        while(tC-- > 0)
+            sb.append(plus123(Integer.parseInt(br.readLine()))+"\n");
+        System.out.println(sb);
     }
     static int plus123(int n) {
         if(dp[n] != 0)
             return dp[n];
-        else {
-            dp[n] = plus123(n-1) + plus123(n-2) + plus123(n-3);
-            return dp[n];
-        }
+        else
+            return dp[n] = plus123(n-1) + plus123(n-2) + plus123(n-3);
     }
 }
