f440de7f62ef191e43a910f346b83846c2cffec9
Seunghyun._.KO <62418972+chris0825@users.noreply.github.com>
2022-03-06 11:16:25 +0900

    Update 11727.java

--- Dynamic Programming/11727.java
+++ Dynamic Programming/11727.java
@@ -10,9 +10,8 @@ import java.io.*;
 public class Main {
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        int dp[] = new int[1001];
+        int dp[] = new int[n+1];
         dp[1] = 1;
         for(int i=2; i<=n; i++) {
             if(i%2 == 0)
@@ -20,7 +19,6 @@ public class Main {
             else
                 dp[i] = (dp[i-1]*2-1)%10007;
         }
-        bw.write(String.valueOf(dp[n]));
-        bw.close();
+        System.out.println(dp[n]);
     }
 }
